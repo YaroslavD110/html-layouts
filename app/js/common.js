d61ed2db29fd5393f96fe6d9dab58edd08736530
tangerine69 <tangerine69@gmail.com>
2018-09-14 15:02:56 +0300

    initial commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,59 @@
-$(function() {
+$(document).ready(function() {
+  // Video popup
+  $(".video-btn").modalVideo();
 
-	// Custom JS
+  // Samples carousel
+  $("#samples-carousel").owlCarousel({
+    items: 3,
+    loop: true,
+    dots: false,
+    center: true,
+    margin: 50,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        center: false
+      },
+      615: {
+        items: 1,
+        nav: true,
+        center: false
+      },
+      768: {
+        items: 2,
+        nav: true,
+        center: false
+      },
+      1150: {
+        items: 3,
+        nav: true
+      }
+    }
+  });
 
+  // Smooth scroll to id
+  $(".scroll-btn").on("click", function(e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      800,
+      "linear"
+    );
+  });
+
+  // Toggle menu
+  $(".header-panel__menu-btn").on("click", toggleMenu);
+  $(".header-panel__menu-link").on("click", toggleMenu);
+
+  function toggleMenu() {
+    $(".header-panel__menu-btn").toggleClass("header-panel__menu-btn__opened");
+    $(".header-panel__menu").slideToggle(300, function() {
+      if ($(this).attr("style") === "display: none;") {
+        $(this).removeAttr("style");
+      }
+    });
+  }
 });
